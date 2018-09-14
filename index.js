34b36f40f5e3037a9b5d2e158091cdac735e084c
Dan Barrick <barrick.dan80@gmail.com>
2018-09-14 23:13:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function increaseRankBy(n) {
 function deepestChild() {
   let node = document.getElementById('grand-node')
   let nextNode = node.children[0]
+}
 
   while (nextNode) {
     node = nextNode
